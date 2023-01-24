e03b303531c9c8d139f5c345cfa56cc26bd0dd10
Masyn N <masynnay33@gmail.com>
2023-01-24 00:41:15 -0700

    Added more code to JS and HTML
    added a recent-city ID to the HTML to be able to manipulate it in JS
    
    Also added more lines in JS to be able to see our LocalStorage Data as buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,45 @@
 var apiKey = "01c96215aa0d4a4be72c357c3513ec88"
 var recentCity = []
 
-
 $(document).ready(function() {
   recentCity = JSON.parse(localStorage.getItem("recentlySearched"))
-  
+  generateRecentCities(recentCity)
   // Search button listener
- $("#citySearchBtn").on("click", function (event) {
-  event.preventDefault()
-  var searchValue = $("#searchedCity").val()
-  console.log(searchValue)
-  // Saves to local storage
-  recentCity.push (searchValue)
-  console.log (recentCity)
-  localStorage.setItem("recentlySearched", JSON.stringify(recentCity))
+  $("#citySearchBtn").on("click", function (event) {
+    event.preventDefault()
+    var searchValue = $("#searchedCity").val()
+    console.log(searchValue)
+    // Saves to local storage
+    recentCity.push (searchValue)
+    localStorage.setItem("recentlySearched", JSON.stringify(recentCity))
+    generateRecentCities(recentCity)
+    getCity(recentCity[recentCity.length-1])
  })
 })
-function getCity(recentCity){
-  var url = ("api.openweathermap.org/data/2.5/forecast?q=" + recentCity + "&appid=" + apiKey)
+function getCity(cityName){
+    var url = ("https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey + "&&units=imperial")
+  
+    // Fetches the api
+    fetch(url)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data)
+      // update the weather on hte page
 
-  fetch(url)
-  .then(function (response) {
-    response.json;
-  })
-  .then(function(data) {
-    console.log("getCity response =", data)
-  })
-}
+      //
+    })
+  }
+  function generateRecentCities(recentCities) {
+    let recentCityElement = $("#recent-city")
+    recentCityElement.empty()
+    for(let i = 0;i <= recentCities.length-1;i++){
+      let button=$('<button></button>')
+      button.text(recentCities[i])
+      button.on("click" ,function(event) {
+        getCity(recentCities[i])
+      })
+      recentCityElement.append(button)
+    }
+  }
